e476b2b56b6755e933ff8271fc1ea7bf7058dd0e
Anikmajumder <53091562+Anikmajumder@users.noreply.github.com>
2020-12-05 20:50:58 +0600

    Add project files.

---
+++ BookHouse.Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BookHouse.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
